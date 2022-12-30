4893376bfdd2a3fcd7d4893e44705fefeb4c1591
Aaron Richards <aaron_richards@hotmail.co.uk>
2022-12-30 20:42:48 +0000

    keep track of hits and not allow duplicates

--- src/index.js
+++ src/index.js
@@ -7,8 +7,3 @@ const board = gameboard(5)
 const ship = newShip(3)
 // ship.getLength()
 
-
-console.log(board.printBoard())
-board.placeShips('a', 1, ship)
-board.placeShips('a', 2, ship)
-console.log(board.printBoard())
