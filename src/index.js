2f8ddb00a251edea10a75777ad41b851d63c1081
Aaron Richards <aaron_richards@hotmail.co.uk>
2023-01-03 17:22:12 +0000

    fixed issue with test

--- src/index.js
+++ src/index.js
@@ -5,15 +5,14 @@ import gameboard from './gameboard';
 
 const board = gameboard(5);
 const ship = newShip(3);
-ship.getLength()
 
-board.placeShips('d', 1, ship);
-board.placeShips('e', 1, ship);
-console.log(board.printBoard());
-console.log(board.checkAlive());
+// board.placeShips('d', 1, ship);
+// board.placeShips('e', 1, ship);
+// console.log(board.printBoard());
+// console.log(board.checkAlive());
 
-board.recieveHit('e1');
-board.recieveHit('e2');
-console.log(board.checkAlive());
-board.recieveHit('e3');
-console.log(board.checkAlive());
+// board.recieveHit('e1');
+// board.recieveHit('e2');
+// console.log(board.checkAlive());
+// board.recieveHit('e3');
+// console.log(board.checkAlive());
