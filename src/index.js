3fc848ab522107e25a83cc731d4982f8a5bd296f
Aaron Richards <aaron_richards@hotmail.co.uk>
2023-01-04 10:03:05 +0000

    all tests working as expected. created Main control of game

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import './style.css';
 import newShip from './ship';
 import gameboard from './gameboard';
+import Player from './player';
+import mainLoop from './mainLoop';
 // import Icon from './icon.png';
 
+const main = mainLoop();
 const board = gameboard(5);
 const ship = newShip(3);
+const player = Player();
 
 // board.placeShips('d', 1, ship);
 // board.placeShips('e', 1, ship);
