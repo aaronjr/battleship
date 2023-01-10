cd71489d869eea408dd03bd053b169083fbb9bd7
Aaron Richards <aaron_richards@hotmail.co.uk>
2023-01-10 20:41:56 +0000

    more small changes

--- webpack.config.js
+++ webpack.config.js
@@ -5,8 +5,8 @@ module.exports = {
   mode: 'development',
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, 'build'),
     filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
   devtool: 'inline-source-map',
