c45a9ecccc008854f206973c5e2b9f99ece3fd4a
Aaron Richards <aaron_richards@hotmail.co.uk>
2023-01-10 20:36:06 +0000

    changes to make github pages work

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'build'),
-    filename: 'build/[name].js',
+    filename: 'bundle.js',
     clean: true,
   },
   devtool: 'inline-source-map',
